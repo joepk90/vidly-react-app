628431359c88a0d9b9c37308cffb9249d5005e2c
Joe <jparkkennaby@gmail.com>
2020-05-02 09:34:44 +0100

    setup key property for mapped elements in TableBody component

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -13,6 +13,10 @@ class TableBody extends Component {
 
     }
 
+    createKey = (item, column) => {
+        return item._id + (column.path || column.key);
+    }
+
     render() {
 
         const { data, columns } = this.props;
@@ -20,8 +24,8 @@ class TableBody extends Component {
         return (
             <tbody>
                 {data.map(item => (
-                    <tr>
-                        {columns.map(column => <td>{this.renderCell(item, column)}</td>)}
+                    <tr key={item._id}>
+                        {columns.map(column => <td key={this.createKey(item, column)} >{this.renderCell(item, column)}</td>)}
                     </tr>
                 ))}
 
