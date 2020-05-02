e78ef9784f847703407d43e0ec7204f20d2f72a4
Joe <jparkkennaby@gmail.com>
2020-05-02 08:01:06 +0100

    update pagination component to use button element instead of link tag for accessibility

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -31,7 +31,7 @@ class Pagination extends Component {
         return pages.map((page) => {
             return (
                 <li key={page} className={this.pageItemClasses(page)}>
-                    <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
+                    <button className="page-link" onClick={() => onPageChange(page)}>{page}</button>
                 </li>
             );
         })
