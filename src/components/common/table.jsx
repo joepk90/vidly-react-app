b48ed19afbf8d6fa630c783de717ae86e0707d72
Joe <jparkkennaby@gmail.com>
2020-05-02 09:57:17 +0100

    create table component - extract movies table logic to resuable table component

---
+++ src/components/common/table.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import TableHeader from './tableHeader';
+import TableBody from './tableBody';
+
+
+const Table = (props) => {
+
+    const { columns, onSort, sortColumn, data } = props;
+
+    return (
+        <table className="table">
+            <TableHeader
+                columns={columns}
+                sortColumn={sortColumn}
+                onSort={onSort}
+            />
+
+            <TableBody
+                data={data}
+                columns={columns}
+            />
+
+        </table>
+    );
+
+}
+
+export default Table;
