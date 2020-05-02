71834631c0b7d688552e8422e11d28c9636122ac
Joe <jparkkennaby@gmail.com>
2020-05-02 08:35:23 +0100

    refactor onSort event in moviesTable component. create a raiseSort method to hand sort logic. pass updated sortColumn object back to the movies component to handle state

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -69,16 +69,7 @@ class Movies extends Component {
         this.setState({ selectedGenre: genre, currentPage: 1 })
     }
 
-    handleSort = path => {
-
-        const sortColumn = { ...this.state.sortColumn };
-
-        if (sortColumn.path === path) {
-            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
-        } else {
-            sortColumn.path = path;
-            sortColumn.order = 'asc';
-        }
+    handleSort = sortColumn => {
 
         this.setState({ sortColumn });
     }
@@ -111,6 +102,7 @@ class Movies extends Component {
 
                     <MoviesTable
                         movies={movies}
+                        sortColumn={sortColumn}
                         onLike={this.handleLike}
                         onDelete={this.handleDelete}
                         onSort={this.handleSort}
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -3,17 +3,33 @@ import Like from "./common/like";
 
 class MoviesTable extends Component {
 
+    raiseSort = path => {
+
+        const sortColumn = { ...this.props.sortColumn };
+
+        console.log(sortColumn);
+
+        if (sortColumn.path === path) {
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+        } else {
+            sortColumn.path = path;
+            sortColumn.order = 'asc';
+        }
+
+        this.props.onSort(sortColumn);
+    }
+
     render() {
 
-        const { movies, onDelete, onLike, onSort } = this.props;
+        const { movies, onDelete, onLike } = this.props;
         return (
             <table className="table">
                 <thead>
                     <tr>
-                        <th onClick={() => onSort('title')} scope="col">Title</th>
-                        <th onClick={() => onSort('genre.name')} scope="col">Genre</th>
-                        <th onClick={() => onSort('numberInStock')} scope="col">Stock</th>
-                        <th onClick={() => onSort('dailyRentalRate')} scope="col">Rate</th>
+                        <th onClick={() => this.raiseSort('title')} scope="col">Title</th>
+                        <th onClick={() => this.raiseSort('genre.name')} scope="col">Genre</th>
+                        <th onClick={() => this.raiseSort('numberInStock')} scope="col">Stock</th>
+                        <th onClick={() => this.raiseSort('dailyRentalRate')} scope="col">Rate</th>
                         <th scope="col"></th>
                         <th scope="col"></th>
                     </tr>
