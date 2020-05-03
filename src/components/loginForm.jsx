0cd9f2df115c95457c45a28c76a3eb9d30609a85
Joe <jparkkennaby@gmail.com>
2020-05-03 21:55:11 +0100

    setup event handler on form onSubmit event and make the username field an contolled element

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,13 +2,41 @@ import React, { Component } from 'react';
 
 class LoginForm extends Component {
 
+    state = {
+        account: {
+            username: '',
+            password: ''
+        }
+
+    }
+
+    handleChange = e => {
+
+        const account = { ...this.state.account };
+
+        account.username = e.currentTarget.value;
+
+        this.setState({ account });
+
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input id="username" type="text" className="form-control" />
+                        <input
+                            autoFocus
+                            value={this.state.account.usename}
+                            onChange={this.handleChange}
+                            id="username" type="text"
+                            className="form-control"
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
