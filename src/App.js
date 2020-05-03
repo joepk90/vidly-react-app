6666da747a63a31e2445e3362c36e8499d8eb74a
Joe <jparkkennaby@gmail.com>
2020-05-03 11:44:20 +0100

    setup application routing. create customers, movieform, navbar, notfound, and rentals components. setup navigation links in navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+
+import NavBar from './components/navbar'
+import MovieForm from './components/movieForm';
 import Movies from './components/movies';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
+import NotFound from './components/notFound';
+
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
+      <NavBar />
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
-    </div >
+    </React.Fragment>
   );
 }
 
