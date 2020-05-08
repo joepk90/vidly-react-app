aa70df8fd7a0f4902788cf6ed01272e1693866af
Joe <jparkkennaby@gmail.com>
2020-05-08 11:34:34 +0100

    improve input component logic. update input component to use spread operator when object destructuring

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const Input = ({ type, name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
 
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input
-                // autoFocus
-                value={value}
-                onChange={onChange}
+                {...rest}
                 id={name}
                 name={name}
-                type={type}
                 className="form-control"
             />
             {error && <div className="alert alert-danger">{error}</div>}
