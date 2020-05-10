75b211939ec427b91033ea1bff003dfe46bae0db
Joe <jparkkennaby@gmail.com>
2020-05-10 19:25:51 +0100

    refactor movieService logic. functionalise movieAPI URL generation

--- src/services/movieService.jsx
+++ src/services/movieService.jsx
@@ -3,13 +3,17 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+    return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
 
     return http.get(apiEndpoint);
 }
 
 export function getMovie(id) {
-    return http.get(`${apiEndpoint}/${id}`);
+    return http.get(movieUrl(id));
 }
 
 export function saveMovie(movie) {
@@ -17,13 +21,13 @@ export function saveMovie(movie) {
     if (movie._id) {
         const body = { ...movie };
         delete body._id;
-        return http.put(`${apiEndpoint}/${movie._id}`, body);
+        return http.put(movieUrl(movie._id), body);
     }
 
-    return http.post(`${apiEndpoint}`, movie);
+    return http.post(apiEndpoint, movie);
 
 }
 
 export function deleteMovie(id) {
-    return http.delete(`${apiEndpoint}/${id}`);
+    return http.delete(movieUrl(id));
 }
