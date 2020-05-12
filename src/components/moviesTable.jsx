d1cb8675451817365126290fde149c1760fe9411
Joe <jparkkennaby@gmail.com>
2020-05-12 21:20:53 +0100

    setup movies component to hide delete column if not logged in and not an admin

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Like from "./common/like";
 import Table from "./common/table";
+import auth from "../services/authService";
 
 class MoviesTable extends Component {
+
+    deleteColumn = {
+        key: 'delete',
+        content: movie => (
+            <button
+                id={movie._id} onClick={() => this.props.onDelete(movie)}
+                type="button"
+                className="btn btn-danger"
+            >Delete</button>
+        )
+    }
 
     columns = [
         { path: 'title', label: 'Title', content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> },
@@ -17,18 +29,15 @@ class MoviesTable extends Component {
                     onClick={() => this.props.onLike(movie)}
                 />
             )
-        },
-        {
-            key: 'delete',
-            content: movie => (
-                <button
-                    id={movie._id} onClick={() => this.props.onDelete(movie)}
-                    type="button"
-                    className="btn btn-danger"
-                >Delete</button>
-            )
-        },
+        }
     ];
+
+    constructor() {
+        super();
+
+        const user = auth.getCurrentUser();
+        if (user && user.isAdmin) this.columns.push(this.deleteColumn)
+    }
 
     render() {
 
